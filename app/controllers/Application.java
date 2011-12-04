736680c6cd8ed7ab53fdac4e7ca5b6138f308021
Honnix Liang <hxliang1982@gmail.com>
2011-12-04 15:27:41 +0800

    the front page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,24 @@
 package controllers;
 
+import java.util.List;
+
+import models.Post;
+import play.Play;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
+    @Before
+    private static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
     public static void index() {
-        System.out.println("hello");
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
 }

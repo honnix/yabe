41c5101467363c5f3a13e3911022316069efa838
Honnix Liang <hxliang1982@gmail.com>
2011-12-16 10:41:05 +0800

    readme

---
+++ README.mk
@@ -0,0 +1,2 @@
+This tutorial is also distributed as a sample application. You can find the final code in your Play installation’s samples-and-tests/yabe/ directory.
+While I will add more stuffs in, like maven integration.
